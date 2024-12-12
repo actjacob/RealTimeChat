4171078596c858c3303bf511db018bcd978520cd
actjacob <yakupaktar99@gmail.com>
2024-12-12 17:01:18 +0300

    Safeare eklenerek içeriğin yanlış yerlere taşması engellendi state denemesi yapıldı

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
-export default function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const add = () => {
+    setCount((prev) => prev + 1);
+    setCount((prev) => prev + 1);
+
+    // setCount(count + 1);
+    // setCount(count + 1);
+  };
+  const minus = () => {
+    setCount(count - 1);
+  };
+
+  console.log("I'm rendering");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style={styles.container}>
+      <SafeAreaView>
+        <StatusBar style="auto" />
+
+        <Button title="Add" onPress={add} />
+
+        <Text style={styles.text}> {count} </Text>
+
+        <Button title="Minus" onPress={minus} />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#3498db",
+    fontSize: 24,
+    textAlign: "center",
   },
 });
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 React Native ve Firebase ile Real Time Chat Uygulaması
+SafeArea
+
+expo install react-native-safe-area-context
