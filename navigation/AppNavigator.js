de39cb32ebb4079e7e202cbc716e6b5e30c85837
actjacob <yakupaktar99@gmail.com>
2024-12-18 15:11:59 +0300

    seperate looged in navigator

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,61 +1,12 @@
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
-import Fontisto from '@expo/vector-icons/Fontisto';
 
-import ChatListScreen from '../screens/ChatListScreen';
-import ChatSettingsScreen from '../screens/ChatSettingsScreen';
-import SettingsScreen from '../screens/SettingsScreen';
-
-const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-const TabNavigator = () => {
-  return (
-    <Tab.Navigator screenOptions={{ headerTitle: '' }}>
-      {/* Burada kök dizinde yapıyoruz başlık göstermeme işlemini */}
-      <Tab.Screen
-        name="ChatList"
-        component={ChatListScreen}
-        tav
-        options={{
-          tabBarLabel: 'Chat',
-          tabBarIcon: ({ color, size }) => {
-            // return <Ionicons name="chatbubble-outline" size={24} color="black" />;
-            return <Fontisto name="hipchat" size={size} color={color} />;
-          },
-        }}
-      />
-      <Tab.Screen
-        name="Settings"
-        component={SettingsScreen}
-        options={{
-          tabBarLabel: 'Setttings',
-          tabBarIcon: ({ color, size }) => {
-            return <Ionicons name="settings-outline" size={size} color={color} />;
-          },
-        }}
-      />
-    </Tab.Navigator>
-  );
-};
+import MainNavigator from './MainNavigator';
 
 const AppNavigator = (props) => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={TabNavigator} options={{ headerShown: false }} />
-        <Stack.Screen
-          name="ChatSettings"
-          component={ChatSettingsScreen}
-          options={{
-            headerTitle: 'Settings',
-            //headerBackTitle: 'Back', androidde çalışmadı ancak iOS emilatorde çalıştı
-            headerShadowVisible: false,
-          }}
-        />
-      </Stack.Navigator>
+      <MainNavigator />
     </NavigationContainer>
   );
 };
---
+++ navigation/MainNavigator.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import Fontisto from '@expo/vector-icons/Fontisto';
+
+import ChatListScreen from '../screens/ChatListScreen';
+import ChatSettingsScreen from '../screens/ChatSettingsScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator screenOptions={{ headerTitle: '' }}>
+      {/* Burada kök dizinde yapıyoruz başlık göstermeme işlemini */}
+      <Tab.Screen
+        name="ChatList"
+        component={ChatListScreen}
+        tav
+        options={{
+          tabBarLabel: 'Chat',
+          tabBarIcon: ({ color, size }) => {
+            // return <Ionicons name="chatbubble-outline" size={24} color="black" />;
+            return <Fontisto name="hipchat" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          tabBarLabel: 'Settings',
+          tabBarIcon: ({ color, size }) => {
+            return <Ionicons name="settings-outline" size={size} color={color} />;
+          },
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+const MainNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={TabNavigator} options={{ headerShown: false }} />
+      <Stack.Screen
+        name="ChatSettings"
+        component={ChatSettingsScreen}
+        options={{
+          headerTitle: 'Settings',
+          //headerBackTitle: 'Back', androidde çalışmadı ancak iOS emilatorde çalıştı
+          headerShadowVisible: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default MainNavigator;
