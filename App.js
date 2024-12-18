493d91bfa078888ee97cb92e37a246fdd028b017
actjacob <yakupaktar99@gmail.com>
2024-12-18 14:35:00 +0300

    Bottom tab  and vector icon add project

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import Fontisto from '@expo/vector-icons/Fontisto';
 
 import ChatListScreen from './screens/ChatListScreen';
 import ChatSettingsScreen from './screens/ChatSettingsScreen';
@@ -19,9 +21,30 @@ const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 const TabNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="ChatList" component={ChatListScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
+    <Tab.Navigator screenOptions={{ headerTitle: '' }}>
+      {/* Burada kök dizinde yapıyoruz başlık göstermeme işlemini */}
+      <Tab.Screen
+        name="ChatList"
+        component={ChatListScreen}
+        tav
+        options={{
+          tabBarLabel: 'Chat',
+          tabBarIcon: ({ color, size }) => {
+            // return <Ionicons name="chatbubble-outline" size={24} color="black" />;
+            return <Fontisto name="hipchat" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          tabBarLabel: 'Setttings',
+          tabBarIcon: ({ color, size }) => {
+            return <Ionicons name="settings-outline" size={size} color={color} />;
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 };
@@ -73,7 +96,7 @@ export default function App() {
       {/* <SafeAreaView> */}
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Home" component={TabNavigator} />
+          <Stack.Screen name="Home" component={TabNavigator} options={{ headerShown: false }} />
           <Stack.Screen
             name="ChatSettings"
             component={ChatSettingsScreen}
