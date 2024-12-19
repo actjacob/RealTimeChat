f11b97fa8e3054e2eed91c6b97b5c0b1b4f3f62b
actjacob <yakupaktar99@gmail.com>
2024-12-19 15:10:53 +0300

    Used SafeAreaView to ensure proper content display within the safe area.

---
+++ components/PageContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const PageContainer = (props) => {
+  return <View style={{ ...styles.container, ...props.style }}>{props.children}</View>;
+};
+
+export default PageContainer;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 20,
+    flex: 1,
+    backgroundColor: 'white',
+    //backgroundColor: '#3498db',
+  },
+});
--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
 import MainNavigator from './MainNavigator';
+import AuthScreen from '../screens/AuthScreen';
+
+const isAuth = false;
 
 const AppNavigator = (props) => {
   return (
     <NavigationContainer>
-      <MainNavigator />
+      {isAuth && <MainNavigator />}
+      {!isAuth && <AuthScreen />}
     </NavigationContainer>
   );
 };
