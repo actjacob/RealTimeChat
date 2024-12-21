a2bae40a56d8418a8d0993ad627262b10cb9706d
actjacob <yakupaktar99@gmail.com>
2024-12-21 20:54:48 +0300

    Prevent  keyboard from hiding the form

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,4 +1,13 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import PageContainer from '../components/PageContainer';
@@ -7,20 +16,38 @@ import SignUpForm from '../components/SignUpForm';
 import SignInForm from '../components/SignInForm';
 import colors from '../constants/colors';
 
+import logo from '../assets/images/logo.png';
+
 const AuthScreen = () => {
   const [isSignUp, setIsSignUp] = useState(false);
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <PageContainer>
-        {isSignUp ? <SignUpForm /> : <SignInForm />}
-        <TouchableOpacity
-          onPress={() => setIsSignUp((prevState) => !prevState)}
-          style={styles.linkContainer}
-        >
-          <Text style={styles.link}>
-            {`Switch to ${isSignUp ? 'Sign in' : 'Sign up'}`}
-          </Text>
-        </TouchableOpacity>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <KeyboardAvoidingView
+            style={styles.keyboardAvoidingView}
+            behavior={Platform.OS === 'ios' ? 'height' : undefined}
+            keyboardVerticalOffset={100}
+          >
+            <View style={styles.imageContainer}>
+              <Image
+                source={logo}
+                style={styles.image}
+                resizeMode="contain"
+              />
+            </View>
+
+            {isSignUp ? <SignUpForm /> : <SignInForm />}
+            <TouchableOpacity
+              onPress={() => setIsSignUp((prevState) => !prevState)}
+              style={styles.linkContainer}
+            >
+              <Text style={styles.link}>
+                {`Switch to ${isSignUp ? 'Sign in' : 'Sign up'}`}
+              </Text>
+            </TouchableOpacity>
+          </KeyboardAvoidingView>
+        </ScrollView>
       </PageContainer>
     </SafeAreaView>
   );
@@ -38,5 +65,16 @@ const styles = StyleSheet.create({
     color: colors.blue,
     fontFamily: 'medium',
     letterSpacing: 0.3,
+  },
+  imageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: '50%',
+  },
+  keyboardAvoidingView: {
+    flex: 1,
+    justifyContent: 'center',
   },
 });
