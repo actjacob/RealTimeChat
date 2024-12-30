fd03fd712f48b18aeeae97ce6cf385d137545c55
actjacob <yakupaktar99@gmail.com>
2024-12-30 23:59:34 +0300

    authSlice added

---
+++ store/authSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const authSlice = createSlice({
+   name: 'auth',
+   initialState: {
+      token: null,
+      userData: null,
+   },
+   reducers: {
+      authenticate: (state, action) => {
+         const { payload } = action;
+         state.token = payload.token;
+      },
+   },
+});
